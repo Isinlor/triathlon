85a9f41482c6c1e52f46fbbc4ae468c754572dd8
Isinlor <isinlor@gmail.com>
2013-05-31 09:53:37 +0200

    Chrome bugfix and code clean - should work now

--- js/index.js
+++ js/index.js
@@ -1,8 +1,10 @@
-whenRady();
+$(document).ready(function(){
+	whenRady()
+});
 
-$(document).ready(whenRady());
-
-$(window).resize(whenResize());
+$(window).resize(function(){
+	whenResize();
+});
 
 $(window).bind('scroll',function(){
     parallaxScroll();
@@ -15,36 +17,29 @@ function whenRady(){
 
 	whenResize();
 
-	var imageBG = new Image();
- 	imageBG.src = $('.background').attr("src");
- 	i = $('.background').attr("src")+ '?v=' + Math.random() ;
+	// var imageBG = new Image();
+	// imageBG.onload = function(){
+	// 	alert(imageBG.height+' '+$(window).width()+' '+imageBG.width +'/'+Math.round((imageBG.height*$(window).width())/imageBG.width));
+	// };
+ 	// 	imageBG.src = $('.background').attr("src")+ '?v=' + Math.random();
 
-	alert(imageBG.height+' '+$(window).width()+' '+imageBG.width +'/'+Math.round((imageBG.height*$(window).width())/imageBG.width));
 	$('.background').height(Math.round((700*$(window).width())/600));
-
-	ratio = countRatio();
-
-	parallaxScroll();
 }
 
 function whenResize(){
 	marginTop = $(window).height() - 2*parseInt($("section header").css('font-size')) - 110;
-	$('.space').css('height', marginTop+'px');
+	$('.container').css('margin-top', marginTop+'px');
 	ratio = countRatio();
-	parallaxScroll();
 }
 
 function parallaxScroll(){
     var scrolled = $(window).scrollTop();
-   	ratio = countRatio();
-    $('.test').text(scrolled +'/WH'+ $(window).height() +'/PH'+ $(document).height() +'/RBGH' + $('.background').height() +'/R' + ratio +'/BGM'+ scrolled*ratio);
+   	var ratio = countRatio();
+    //$('.test').text(scrolled +'/WH'+ $(window).height() +'/PH'+ $(document).height() +'/RBGH' + $('.background').height() +'/R' + ratio +'/BGM'+ scrolled*ratio);
     $('.background').css('top', Math.round(scrolled*ratio)+'px');
 }
+
 // for element fixed
 function countRatio () {
 	return -( $('.background').height() - $(window).height()) / ( $(document).height() - $(window).height() );
 }
-// for element absolute
-// function countRatio () {
-// 	return ( $(document).height() - $('.background').height() ) / ( $(document).height() - $(window).height() );
-// }
