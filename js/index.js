641979aab48d7b9bc3803697aba301349749980f
Isinlor <isinlor@gmail.com>
2013-05-31 08:28:19 +0200

    Reorganization of index.js

--- js/index.js
+++ js/index.js
@@ -1,41 +1,42 @@
-$(document).ready(function () {
- 	var imageBG = new Image();
- 	imageBG.src = $('.background').attr("src");
- 	i = $('.background').attr("src");
+whenRady();
 
-	alert(imageBG.height+' '+$(window).width()+' '+imageBG.width +'/'+Math.round((imageBG.height*$(window).width())/imageBG.width));
-	$('.background').height(Math.round((700*$(window).width())/600));
-	ratio = countRatio();
+$(document).ready(whenRady());
 
-	marginTop = $(window).height() - 2*parseInt($("section header").css('font-size')) - 110;
-	$('.container').css('margin-top', marginTop+'px');
-	ratio = countRatio();
-});
-
-$('section').css('margin-bottom', $(window).height()/1.4+'px');
-
-$("section header").fitText(0.37, { minFontSize: '85px', maxFontSize: '132px' })
-
-marginTop = $(window).height() - 2*parseInt($("section header").css('font-size')) - 110;
-$('.container').css('margin-top', marginTop+'px');
-
- if($(window).height() > $('.background').height()){
-	$('.background').css('height', $(window).height());
- }
-
-var ratio = countRatio();
-
-$(window).resize(function(){
-	//$('section').css('min-height', $(window).height()+'px');
-	marginTop = $(window).height() - 2*parseInt($("section header").css('font-size')) - 110;
-	$('.container').css('margin-top', marginTop+'px');
-	ratio = countRatio();
-});
+$(window).resize(resize());
 
 $(window).bind('scroll',function(){
     parallaxScroll();
 });
  
+function whenRady(){
+	$('section').css('margin-bottom', $(window).height()/1.4+'px');
+
+	$("section header").fitText(0.37, { minFontSize: '85px', maxFontSize: '132px' })
+
+	marginTop = $(window).height() - 2*parseInt($("section header").css('font-size')) - 110;
+	$('.container').css('margin-top', marginTop+'px');
+
+	ratio = countRatio();
+
+	var imageBG = new Image();
+ 	imageBG.src = $('.background').attr("src");
+ 	i = $('.background').attr("src");
+
+	alert(imageBG.height+' '+$(window).width()+' '+imageBG.width +'/'+Math.round((imageBG.height*$(window).width())/imageBG.width));
+	$('.background').height(Math.round((700*$(window).width())/600));
+
+	ratio = countRatio();
+
+	parallaxScroll();
+}
+
+function resize(){
+	marginTop = $(window).height() - 2*parseInt($("section header").css('font-size')) - 110;
+	$('.container').css('margin-top', marginTop+'px');
+	ratio = countRatio();
+	parallaxScroll();
+}
+
 function parallaxScroll(){
     var scrolled = $(window).scrollTop();
     //$('.test').text(scrolled +'/WH'+ $(window).height() +'/PH'+ $(document).height() +'/RBGH' + $('.background').height() +'/R' + ratio +'/BGM'+ scrolled*ratio);
